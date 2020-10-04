07a17f0407f8e757bf53cd1e77408bbd310f7b23
trieshard <trieshard@simaos.net>
2020-10-04 20:26:23 +0100

    fix node path issue

--- ci.sh
+++ ci.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 set -e
-npm --local install shellcheck markdownlint-cli cspell write-good
-PATH="$(npm bin):$PATH"
+module_path_prefix="./.ci_temp"
+npm --local --prefix "${module_path_prefix}" install shellcheck markdownlint-cli cspell write-good
+PATH="${module_path_prefix}/node_modules/.bin:${PATH}"
 shellcheck ./*.sh
 cspell ./*.md
 markdownlint ./*.md
